0823d5c3ee23931dd8cbecb2dc9aa83dc7dce26e
Bellend Sebastian <bellend@bellendsebastian.com>
2012-11-28 15:32:08 +0000

    Progress

--- js/script.js
+++ js/script.js
@@ -1,18 +1,30 @@
 (function () {
 
     var apiKey = "653FB167380DECA9D5C4B8C61409F7FD";
+    var tUserId = "76561197960390219";
 
     lookupUserId = function(username) {
-        var userId = "76561197960390219";
-        return userId;
+        var url = "http://steamcommunity.com/id/" + username + "/?xml=1";
+        $.ajax(url, {
+            type: "GET",
+            dataType: "xml",
+            success: function(request, status) {
+                console.log(request);
+                return tUserId;
+            }
+        });
     };
 
     getGames = function(userId) {
-        var url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=" + apiKey + "&steamids=" + userId;
+        var url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=" + apiKey + "&steamids=" + tUserId;
         $.ajax(url, {
             type: "GET",
+            dataType: "jsonp",
+            jsonp: "jsonp",
             success: function(data) {
-                console.log(data);
+                var player = data.response.players[0];
+                $("body").append("<h1>" + player.personaname + "</h1>");
+                $("body").append("<img src='" + player.avatarfull + "'/>");
             }
         });
     };
