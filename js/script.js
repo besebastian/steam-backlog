c39c6ec5e233fc04463aba5ac1a16388f3b20dc4
Bellend Sebastian <bellend@bellendsebastian.com>
2012-11-29 21:51:40 +0000

    Added bootstrap, made it actually do something.

--- js/script.js
+++ js/script.js
@@ -1,35 +1,74 @@
 (function () {
 
-    var apiKey = "653FB167380DECA9D5C4B8C61409F7FD";
-    var tUserId = "76561197960390219";
+    var user = {};
+    var games = [];
 
-    lookupUserId = function(username) {
-        var url = "http://steamcommunity.com/id/" + username + "/?xml=1";
-        $.ajax(url, {
-            type: "GET",
-            dataType: "xml",
-            success: function(request, status) {
-                console.log(request);
-                return tUserId;
+    lookupUser = function(username) {
+        user = {};
+        var jUrl =  "http://query.yahooapis.com/v1/public/yql?q=use%20'https%3A%2F%2Fgithub.com%2Fdisavian%2F" +
+                    "yql-tables%2Fraw%2Fmaster%2Fsteam%2Fsteam.community.player.profile.xml'%20as%20steam.community" +
+                    ".player.profile%3B%20select%20*%20from%20steam.community.player.profile%20where%20customurl%3D'" +
+                    username + "'&format=json";
+        $.ajax({
+            url: jUrl,
+            dataType: 'jsonp',
+            success: function(data) {
+                user = data.query.results.profile;
+                getGames(username);
             }
         });
     };
 
-    getGames = function(userId) {
-        var url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=" + apiKey + "&steamids=" + tUserId;
-        $.ajax(url, {
-            type: "GET",
-            dataType: "jsonp",
-            jsonp: "jsonp",
+    getGames = function(username) {
+        games = [];
+        var t = "http://pipes.yahoo.com/pipes/pipe.run?Username=" + username + "&_id=3590b205b2df8b2e9376fdd7e4e9a969&_render=json";
+        $.ajax({
+            url: t,
             success: function(data) {
-                var player = data.response.players[0];
-                $("body").append("<h1>" + player.personaname + "</h1>");
-                $("body").append("<img src='" + player.avatarfull + "'/>");
+                games = data.value.items[0].games.game;
+                displayGames(games);
             }
         });
+    };
+
+    countUnplayed = function(gameList) {
+        var c = 0;
+        $(gameList).each(function(i,v) {
+            if (this.hoursOnRecord === undefined) {
+                c++;
+            }
+        });
+        return c;
+    };
+
+    checkContainer = function() {
+        $(".for-content").fadeOut(0);
+        $(".for-content").html('<div class="row content"><header><h2></h2><p class="small"></p></header><div class="well"><ul class="game-list"></ul></div></div>');
+        return $(".content");
+    };
+
+    displayGames = function(gameList) {
+        var unplayed = countUnplayed(gameList);
+        var percentage = Math.ceil(unplayed / gameList.length * 100);
+        var container = checkContainer();
+
+        container.find("header").prepend("<img src='" + user.avatarIcon + "' alt='Profile image for " + user.steamID + "' class='pull-left' />");
+        container.find("header").children("h2").html("Results for <a href='http://steamcommunity.com/id/" + user.customURL + "' target='_blank'>" + user.steamID + "</a> (" + user.customURL + ")");
+        container.find("header").children("p").html(gameList.length + " game(s) found, out of which " + unplayed + " (" + percentage + "%) are unplayed.");
+
+        $(gameList).each(function(i,v) {
+            if (this.hoursOnRecord === undefined) {
+                container.find(".game-list").append("<li>" + this.name + "</li>");
+            }
+        });
+        $(".for-content").fadeIn(400);
     };
 
     window.init = function() {
-        getGames(lookupUserId("esunub"));
+        $("#steamId").on("submit", function(e) {
+            e.preventDefault();
+            lookupUser(escape($("#username").val()));
+        });
     };
+
 }).call(this);
